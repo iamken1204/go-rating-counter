109f2b4854b3f6e79798fff23db080ab1ad4dd59
Kettan <iamken1204@gmail.com>
2015-12-24 11:44:04 +0800

    Added hint of empty targets

--- core.go
+++ core.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	rc "github.com/iamken1204/rating-counter"
 )
 
 func main() {
 	targets := rc.GetTargets()
+	if len(targets) == 0 {
+		fmt.Println("No targets to be crawled.")
+		os.Exit(1)
+	}
 	for _, target := range targets {
 		rc.Crawl(target.ID, "google", target.Keyword, target.Url)
 		rc.Crawl(target.ID, "yahoo", target.Keyword, target.Url)
